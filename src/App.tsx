f48d1333dae1c8c33ede2c800b8c6ecf9effea21
Yaroslav Petrukhin <yar.petrukhin@gmail.com>
2023-06-21 08:27:03 +0300

    experimental:routing w/o lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React, { lazy, Suspense } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
 // import { fetchProducts } from 'store/products';
 // import { fetchOrders } from 'store/orders';
 // import { useAppDispatch } from 'hooks';
-// import { Layout } from 'components/layout';
+import { Layout } from 'components/layout';
+import { HomePage, OrdersPage, ProductsPage } from './pages';
 
-const HomePage = lazy(() => import('./pages/HomePage'));
-const OrdersPage = lazy(() => import('./pages/OrdersPage'));
-const ProductsPage = lazy(() => import('./pages/ProductsPage'));
+// const HomePage = lazy(() => import('./pages/HomePage'));
+// const OrdersPage = lazy(() => import('./pages/OrdersPage'));
+// const ProductsPage = lazy(() => import('./pages/ProductsPage'));
 
 const App: React.FC = () => {
   // const dispatch = useAppDispatch();
@@ -19,16 +20,16 @@ const App: React.FC = () => {
 
   return (
     <>
-      <Suspense fallback={<h2>LOADING...</h2>}>
-        <Routes>
-          {/* <Route path="/" element={<Layout />}> */}
+      {/* <Suspense fallback={<h2>LOADING...</h2>}> */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="orders" element={<OrdersPage />} />
           <Route path="products" element={<ProductsPage />} />
-          {/* </Route> */}
-          {/* <Route path="/*" element={<Navigate to="/" />} /> */}
-        </Routes>
-      </Suspense>
+        </Route>
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
+      {/* </Suspense> */}
     </>
   );
 };
---
+++ src/pages/index.ts
@@ -0,0 +1,3 @@
+export { default as HomePage } from './HomePage';
+export { default as OrdersPage } from './OrdersPage';
+export { default as ProductsPage } from './ProductsPage';
