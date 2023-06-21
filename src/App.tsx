13574cf1d2a9966db7f0e38c93de2f2f94a097ed
Yaroslav Petrukhin <yar.petrukhin@gmail.com>
2023-06-21 15:48:16 +0300

    lazy loading added

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,31 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-// import { MoonLoader } from 'react-spinners';
+import { MoonLoader } from 'react-spinners';
 import 'react-toastify/dist/ReactToastify.css';
 import { Layout } from 'components/layout';
-import { HomePage, OrdersPage, ProductsPage } from './pages';
 
-// const HomePage = lazy(() => import('./pages/HomePage'));
-// const OrdersPage = lazy(() => import('./pages/OrdersPage'));
-// const ProductsPage = lazy(() => import('./pages/ProductsPage'));
+const HomePage = lazy(() => import('./pages/HomePage'));
+const OrdersPage = lazy(() => import('./pages/OrdersPage'));
+const ProductsPage = lazy(() => import('./pages/ProductsPage'));
 
 const App: React.FC = () => {
   return (
     <>
-      {/* <Suspense
+      <Suspense
         fallback={
           <MoonLoader color="#2E7758" cssOverride={{ margin: '164px auto' }} size="100px" />
         }
-      > */}
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/orders" element={<OrdersPage />} />
-          <Route path="/products" element={<ProductsPage />} />
-        </Route>
-        <Route path="/*" element={<Navigate to="/" />} />
-      </Routes>
-      {/* </Suspense> */}
+      >
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/orders" element={<OrdersPage />} />
+            <Route path="/products" element={<ProductsPage />} />
+          </Route>
+          <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
+      </Suspense>
       <ToastContainer theme="light" position="top-center" autoClose={5000} />
     </>
   );
